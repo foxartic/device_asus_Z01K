a8ab8554def1470881db671e332b4120a42ef0a2
foxartic <harjeet.singh5101@gmail.com>
2024-07-01 18:18:08 +0530

    reupload

---
+++ AndroidProducts.mk
@@ -0,0 +1,15 @@
+
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_Z01KD_1.mk
+    
+    
+COMMON_LUNCH_CHOICES := \
+    lineage_Z01KD_1-user \
+    lineage_Z01KD_1-userdebug \
+    lineage_Z01KD_1-eng
---
+++ BoardConfig.mk
@@ -0,0 +1,90 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/asus/Z01KD_1
+BOARD_VENDOR := asus
+
+#ROM-HACKS
+BUILD_BROKEN_DUP_RULES := true
+
+#ARCHITECTURE
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic 
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+
+#BOARD SEPOLICY
+BOARD_SEPOLICY_VERS=30.0
+
+#BOOTLOADER 
+TARGET_BOOTLOADER_BOARD_NAME := Z01KD_1
+TARGET_NO_BOOTLOADER := true
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1080
+TARGET_SCREEN_WIDTH := 1920
+
+#FSTAB
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom 
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
+#KERNEL
+BOARD_KERNEL_BASE := 0x00008000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_TAGS_OFFSET := 0x00008000
+BOARD_RAMDISK_OFFSET := 0x01000000 
+BOARD_TAGS_OFFSET := 0x00000100
+BOARD_KERNEL_IMAGE_NAME := zImage
+
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8
+BOARD_KERNEL_CMDLINE := androidboot.console=ttyMSM0 androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE := androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3 
+BOARD_KERNEL_CMDLINE := earlycon=msm_serial_dm,0xc170000 msm_rtb.filter=0x37 ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := sched_enable_hmp=1 sched_enable_power_aware=1
+BOARD_KERNEL_CMDLINE := lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=1
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
+
+#PARTITION
+# Partitions - Boot
+BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
+BOARD_FLASH_BLOCK_SIZE := 262144
+
+# Partitions - Cache
+BOARD_CACHEIMAGE_PARTITION_SIZE := 134217728
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Partitions - Recovery
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
+
+# Partitions - System
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4831838208
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Partitions - Userdata
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 56465112576
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+
+#PLATFORM
+TARGET_BOARD_PLATFORM := sdm630
+
+#SEPOLICY
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIR += device/qcom/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIR += device/qcom/sepolicy/public
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
+#VINTF
+PRODUCT_ENFORCE_VINTF_MANIFEST=false
+-include vendor/asus/Z01KD_1/BoardConfigVendor.mk
